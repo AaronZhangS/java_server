c539419b55ca9f974ff14cadfd372851c2a5db35
zhangsheng <zhangsheng@ac361.com>
2020-04-13 16:39:35 +0800

    修改战队排名排序问题

--- src/main/java/com/example/api/TestApi.java
+++ src/main/java/com/example/api/TestApi.java
@@ -1,5 +1,6 @@
 package com.example.api;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,19 @@ public class TestApi {
 	@RequestMapping("/rank")
 	@CrossOrigin
 	public List<Rank> rank(){
-		return rankServiceImpl.allRank();
+		List<Rank> defultlist = rankServiceImpl.allRank();
+		for(int i = 0;i < defultlist.size();i++) {
+			for(int j = 0;j < defultlist.size()-i-1;j++) {
+				if(Integer.parseInt(defultlist.get(j).getPaiming())>Integer.parseInt(defultlist.get(j+1).getPaiming())) {
+					defultlist.set(j, defultlist.get(j+1));
+					defultlist.set(j+1, defultlist.get(j));
+				}
+			}
+		}
+		for(int i = 0;i < defultlist.size();i++) {
+			defultlist.get(i).setId(i+1);
+		}
+		return defultlist;
 	}
 	@RequestMapping("/webhook")
 	@CrossOrigin
